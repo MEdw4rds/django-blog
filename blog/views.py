adbc6ca44b69f5dd19c9751a77fe42bc365bb53b
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-08 09:38:04 +0000

    bugfix corrected post_details to post_detail

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
-def post_details(request, slug):
+def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
 
