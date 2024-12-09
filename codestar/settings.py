c2d615b53aa09781525a54298ad1de9d56434841
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-09 15:52:42 +0000

    add image serving in about

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-medw4rds-djangoblog-qyj4acyd9ji.ws.codeinstitute-ide.net',
 '.herokuapp.com']
