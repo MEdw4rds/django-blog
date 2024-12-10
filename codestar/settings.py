192c5c90438fb70e5339a611855e3e637abb233f
Matthew Edwards <wste993l@students.codeinstitute.net>
2024-12-10 10:00:32 +0000

    HTML Validatio

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-medw4rds-djangoblog-qyj4acyd9ji.ws.codeinstitute-ide.net',
 '.herokuapp.com']
